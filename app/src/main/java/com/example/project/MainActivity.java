f13d9e598eaa2d5010c7d1d20d3a3b73c1aa00ce
a22jacka <a22jacka@student.his.se>
2023-05-09 15:38:31 +0200

    added URL and json handling

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -12,8 +12,17 @@ import android.webkit.WebViewClient;
 
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
     WebView webview;
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22jacka";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         webview = findViewById(R.id.aboutWebView);
         webview.setWebViewClient(new WebViewClient());
         webview.getSettings().setJavaScriptEnabled(true);
+
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
@@ -37,5 +49,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         webview.loadUrl("file:///android_asset/web/about.html");
         return true;
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Cat>>() {}.getType();
+        List<Cat> catList = gson.fromJson(json, type);
+        //ArrayList<RecyclerItem> items = new ArrayList<>();
     }
 }
